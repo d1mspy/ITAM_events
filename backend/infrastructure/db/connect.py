470a788498ce3fb6e8ca38bb68b30785cdab7e8f
origin <lyil.x.d.v@yandex.ru>
2024-11-28 02:17:44 +0300

    -

--- backend/infrastructure/db/connect.py
+++ backend/infrastructure/db/connect.py
@@ -1,9 +1,13 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+import sqlite3
 
 # подключение к sqlite БД для событий
-def sqlite_connection() -> async_sessionmaker[AsyncSession]:
+def sqlite_connection() -> async_sessionmaker[AsyncSession] | None:
     
     url = "sqlite+aiosqlite:///backend/sqlite.db"
 
-    engine = create_async_engine(url, connect_args={"check_same_thread": False})
-    return async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    try:
+        engine = create_async_engine(url, connect_args={"check_same_thread": False})
+        return async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    except sqlite3.DatabaseError:
+        return None 
