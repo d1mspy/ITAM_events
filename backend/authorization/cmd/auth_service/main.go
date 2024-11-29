d8568b2feee9348fd342fb5439eff98a47f9cfb6
Tuygunova Ekaterina <klarness@list.ru>
2024-11-29 16:48:09 +0300

    авторизация

--- backend/authorization/cmd/auth_service/main.go
+++ backend/authorization/cmd/auth_service/main.go
@@ -11,23 +11,20 @@ import (
 )
 
 func main() {
-	// Load configuration from config.json
+
 	cfg, err := config.LoadConfig("config.json")
 	if err != nil {
 		log.Fatalf("Could not load config: %v", err)
 	}
 
-	// Initialize JWT secret key from config.
 	utils.InitializeSecretKey(cfg.SecretKeys.JWTSecret)
 
-	// Initialize database connection and run migrations using loaded config.
 	if err := database.InitDB(cfg); err != nil {
 		log.Fatalf("Could not initialize database: %v", err)
 	}
 
 	e := echo.New()
 
-	// Define routes for registration and login.
 	e.POST("/register", services.RegisterUser)
 	e.POST("/login", services.LoginUser)
 
