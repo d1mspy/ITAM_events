b890e6e4be715b3633d4965e7de86b9cedb6ca78
d1mspy <rom.dog20@gmail.com>
2024-12-01 22:10:23 +0300

    add pydantic model
    create class Event(BaseModel)
    change endpoints
    add CORS middleware

--- backend/presentations/app.py
+++ backend/presentations/app.py
@@ -1,43 +1,71 @@
 from fastapi import FastAPI, Path, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from repositories.db.event_repository import EventRepository
 from sqlalchemy.exc import OperationalError, ArgumentError
 from datetime import datetime
+from pydantic import BaseModel
 
 app = FastAPI(
     title="ITAM мероприятия",
     description="микросервис для управления мероприятиями на платформе ITAM"
 )
 
-event = EventRepository()
+# экземпляр класса для взаимодействия с базой данных
+event_rep = EventRepository()
+
+# класс мероприятия
+class Event(BaseModel):
+    name: str
+    start_year: int
+    start_month: int
+    start_day: int
+    start_hour: int
+    start_minute: int
+    end_year: int
+    end_month: int
+    end_day: int
+    end_hour: int
+    end_minute: int
+    place: str
+    content: str | None
+    category: str | None
+    tags: str | None
+    
+
+# разрешение запросов из сторонних сервисов(для фронтенда)
+def allow_requests() -> None:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
 
 
-@app.post("/events")
-async def post_event(name: str,
-                     start_year: int, start_month: int, start_day: int, start_hour: int, start_minute: int, 
-                     end_year: int, end_month: int, end_day: int, end_hour: int, end_minute: int, 
-                     place: str, content: str, category: str, tags: str) -> None:
+@app.post("/")
+async def post_event(event: Event) -> None:
     """
     создание мероприятия
     """
 
-    start_datetime = datetime(start_year, start_month, start_day, start_hour, start_minute)
-    end_datetime = datetime(end_year, end_month, end_day, end_hour, end_minute)
+    start_datetime = datetime(event.start_year, event.start_month, event.start_day, event.start_hour, event.start_minute)
+    end_datetime = datetime(event.end_year, event.end_month, event.end_day, event.end_hour, event.end_minute)
     
     try:
-        await event.post_event(name, start_datetime, end_datetime, place, content, category, tags)
+        await event_rep.post_event(event.name, start_datetime, end_datetime, event.place, event.content, event.category, event.tags)
     except OperationalError:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Отсутствует база данных или соответствующее поле")
 
 
-
-@app.get("/events/{id}")
+@app.get("/{id}")
 async def get_event(id: str = Path(...)) -> dict | None:
     """
     получение информации о мероприятии по id
     """
     
     try:
-        info = await event.get_event(id)
+        info = await event_rep.get_event(id)
     except OperationalError:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Отсутствует база данных или соответствующее поле")
 
@@ -47,50 +75,44 @@ async def get_event(id: str = Path(...)) -> dict | None:
     return info
 
 
-
-@app.put("/events/{id}")
-async def put_event(name: str,
-                    start_year: int, start_month: int, start_day: int, start_hour: int, start_minute: int, 
-                    end_year: int, end_month: int, end_day: int, end_hour: int, end_minute: int, 
-                    place: str, content: str, category: str, tags: str, id: str = Path(...)) -> None:
+@app.put("/{id}")
+async def put_event(event: Event, id: str = Path(...)) -> None:
     """
     обновление информации о мероприятии
     """
 
-    start_datetime = datetime(start_year, start_month, start_day, start_hour, start_minute)
-    end_datetime = datetime(end_year, end_month, end_day, end_hour, end_minute)
+    start_datetime = datetime(event.start_year, event.start_month, event.start_day, event.start_hour, event.start_minute)
+    end_datetime = datetime(event.end_year, event.end_month, event.end_day, event.end_hour, event.end_minute)
 
     try:
-        await event.put_event(id, name, start_datetime, end_datetime, place, content, category, tags)
+        await event_rep.put_event(id, event.name, start_datetime, end_datetime, event.place, event.content, event.category, event.tags)
     except OperationalError:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Отсутствует база данных или соответствующее поле")
     except ArgumentError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Мероприятие не найдено")
 
 
-
-@app.delete("/events/{id}")
+@app.delete("/{id}")
 async def delete_event(id: str = Path(...)) -> None:
     """
     Удаление мероприятия
     """
     try:
-        await event.delete_event(id)
+        await event_rep.delete_event(id)
     except OperationalError:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Отсутствует база данных или соответствующее поле")
     except ArgumentError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Мероприятие не найдено")
 
 
-
-@app.get("/events")
+@app.get("/")
 async def get_all_events() -> list | None:
     """
     Получение информации о всех мероприятиях
     """
     
     try:
-        info = await event.get_all_events()
+        info = await event_rep.get_all_events()
     except OperationalError:
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Отсутствует база данных или соответствующее поле")
     
