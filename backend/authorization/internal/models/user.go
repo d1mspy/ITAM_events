3b4028484ac0ca5a13222eb6c2ae600bf8278481
Tuygunova Ekaterina <klarness@list.ru>
2024-12-06 18:27:19 +0300

    updated token and bd

--- backend/authorization/internal/models/user.go
+++ backend/authorization/internal/models/user.go
@@ -1,11 +1,12 @@
 package models
 
 type User struct {
-    Email     string `json:"email"`
-    Password  string `json:"password"`
-    FirstName string `json:"first_name"`
-    LastName  string `json:"last_name"`
-    Group     string `json:"group"` 
-    Age       int    `json:"age"`   
-    IsAdmin   bool   `json:"is_admin"` 
+	ID             string
+	Email          string
+	HashedPassword string
+	FirstName      string
+	LastName       string
+	Age            int
+	GroupInfo      string
+	IsAdmin        bool
 }
