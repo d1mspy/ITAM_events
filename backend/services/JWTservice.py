e7dc23fbf084d1de8bc9d032c222e5984ba0b038
origin <lyil.x.d.v@yandex.ru>
2024-12-11 23:01:35 +0300

    -

--- backend/services/JWTservice.py
+++ backend/services/JWTservice.py
@@ -4,19 +4,19 @@ from jwt.exceptions import InvalidTokenError
 from config.config import JWT_SECRET
 
 async def check_access_token(authorization_header: str) -> dict:
-    
     if authorization_header is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="пользователь не авторизован")
 
     elif 'Bearer ' not in authorization_header:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="некорректный authorization header")
     
-    token = str(authorization_header.replace('Bearer ', '')).strip()
-    print(token)
-    try:
+    token = encode(payload={"header": str(authorization_header.replace('Bearer ', '')).strip()}, key=JWT_SECRET, algorithm="HS256")
+
+    payload = decode(jwt=token, key=JWT_SECRET, algorithms=["HS256"])
+    """try:
         payload = decode(jwt=token, key=JWT_SECRET, algorithms=["HS256"])
     except InvalidTokenError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="токен не валиден")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="токен не валиден")"""
 
     data = {"id": payload.get("id"), "is_admin": payload.get("is_admin")}
     
