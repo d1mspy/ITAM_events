57585610e2f4dd55312cffe060ee9dd623d19ff9
Polina1heg <Polly29.01.03@gmail.com>
2024-12-12 03:02:53 +0300

    events

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,24 +3,24 @@
 	import { goto } from "$app/navigation";
 	import { apiUrl } from "$lib/index";
 	import axios from "axios";
-	let objects = [];
-	$: tag = [...new Set(objects.map(obj => obj.tags))];
-	$: catigory = [...new Set(objects.map(obj => obj.catigory))];
-	let selectedTag = "";
-	let selectedCatigory = "";
-	$: FilterEvents = selectedTag ? objects.filter(obj => obj.tags === selectedTag) : objects;
-
+	let objects: any[];
+	objects = [];
 	async function fetchObjects() {
 		try {
-			const response = await axios.get(`${apiUrl}`, {
-				withCredentials: true,
+			const response = await fetch(`${apiUrl}`, {
+				credentials: "same-origin",
+				method: "GET",
 				headers: { "Content-Type": "application/json" }
 			});
-			objects = await response.data();
+			objects = await response.json();
 		} catch (error) {
 			console.error("Ошибка загрузки", error);
 			throw error;
 		}
+	}
+	let activeButton: string = "events_main";
+	function setActiveButton(button: string) {
+		activeButton = button;
 	}
 
 	onMount(async () => {
@@ -31,13 +31,16 @@
 		goto("/create_event");
 	}
 
-	function goToObjectPage(id) {
+	function goToObjectPage(id: number) {
 		goto(`/${id}`);
 	}
+	function goToObjectPageMore(id: number) {
+		goto(`/${id}/more`);
+	}
 
-	async function deleteEvent(id) {
+	async function deleteEvent(id: number) {
 		try {
-			const response = await fetch(`${apiUrl}${id}`, {
+			const response = await fetch(`${apiUrl}/${id}`, {
 				method: "DELETE",
 				headers: { "Content-Type": "application/json" }
 			});
@@ -55,102 +58,146 @@
 
 <header>
 	<div class="header">
-		<div class="logo"><img src="logo.svg" /></div>
+		<div class="logo"><img src="logo.svg" alt="ITAM" /></div>
 		<nav>
-			<button class="nav-btn">Мероприятия</button>
-			<button class="nav-btn">Админ-панель</button>
-			<div class="avatar"><img src="avatar.svg" /></div>
+			<button
+				class="nav-btn {activeButton === 'events_main' ? 'active' : ''}"
+				on:click={() => setActiveButton("events_main")}>Мероприятия</button
+			>
+			<button
+				class="nav-btn {activeButton === 'admin' ? 'active' : ''}"
+				on:click={() => setActiveButton("admin")}>Админ-панель</button
+			>
+			<div class="avatar"><img src="avatar.svg" alt="avatar" /></div>
 		</nav>
 	</div>
 </header>
+
 <main>
-	<h1>Админ-панель</h1>
-	<div class="container">
-		<div class="filter-bar">
-			<select bind:value={selectedTag}>
-				<option selected value="">Выбрать категорию</option>
-				{#each tag as tags}
-					<option value={tags}>{tags}</option>
-				{/each}
-			</select>
-			<select>
-				<option selected>Формат проведения</option>
-				<option>Онлайн</option>
-				<option>Офлайн</option>
-			</select>
-			<img src="search.svg" />
-			<input type="text" placeholder="Поиск..." />
+	{#if activeButton === "admin"}
+		<h1>Админ-панель</h1>
+		<div class="container">
+			<div class="controls">
+				<div class="dropdown">
+					<select>
+						<option>Выбрать категорию</option>
+					</select>
+				</div>
+				<div class="dropdown">
+					<select>
+						<option>Формат проведения</option>
+					</select>
+				</div>
+				<img src="search.svg" alt="Поиск" />
+				<input type="text" placeholder="Поиск" style="border: none;" />
+			</div>
+			<button on:click={goToAddPage} class="goAdd">+ Добавить событие</button>
 		</div>
-		<button on:click={goToAddPage} class="goAdd">+ Добавить событие</button>
-	</div>
-	<table class="super_title">
-		<thead>
-			<tr>
-				<th>Название</th>
-				<th>Дата события</th>
-				<th>Статус</th>
-				<th>Изменить</th>
-			</tr>
-		</thead>
-	</table>
-
-	<!-- <table>
-	<tbody>
-        <tr>
-          <td>Основы эффективной разработки</td>
-          <td>01.12.24</td>
-          <td>
-            <select>
-              <option>Опубликовано</option>
-            </select>
-          </td>
-          <td>
-            <img src="edit.svg" style="margin-right: 38px;" class="edit" on:click={() => goToObjectPage(obj.id)}>
-			<img src="trash.svg" class="trash" on:click={()=>deleteEvent(obj.id)}>
-          </td>
-        </tr>
-      </tbody>
-</table> -->
-
-	<table>
-		<tbody>
-			{#each objects as obj (obj.id)}
+		<table>
+			<thead class="super_title">
 				<tr>
-					<td class="event-tag">{obj.name}</td>
-					<td>{obj.start_datetime}</td>
-					<td>
-						<select>
-							<option>Опубликовано</option>
-						</select>
-					</td>
-					<td>
-						<img
-							src="edit.svg"
-							style="margin-right: 38px;"
-							class="edit"
-							on:click={() => goToObjectPage(obj.id)}
-						/>
-						<img src="trash.svg" class="trash" on:click={() => deleteEvent(obj.id)} />
-					</td>
+					<th style="border-radius: 50px 0 0 50px;">Название</th>
+					<th>Дата события</th>
+					<th>Статус</th>
+					<th style="border-radius: 0px 50px 50px 0px;">Изменить</th>
 				</tr>
-			{/each}
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				{#each objects as obj (obj.id)}
+					<tr>
+						<td class="event-tag">{obj.name}</td>
+						<td>{obj.start_datetime}</td>
+						<td>
+							<select>
+								<option>Опубликовано</option>
+							</select>
+						</td>
+						<td>
+							<button on:click={() => goToObjectPage(obj.id)}>
+								<img src="edit.svg" style="margin-right: 38px;" class="edit" alt="edit" />
+							</button>
+							<button on:click={() => deleteEvent(obj.id)}>
+								<img src="trash.svg" class="trash" alt="trash" /></button
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{:else if activeButton === "events_main"}
+		<h1>Мероприятия</h1>
+		<div class="container">
+			<div class="controls">
+				<div class="dropdown">
+					<select>
+						<option>Выбрать категорию</option>
+					</select>
+				</div>
+				<div class="dropdown">
+					<select>
+						<option>Формат проведения</option>
+					</select>
+				</div>
+				<img src="search.svg" alt="Поиск" />
+				<input type="text" placeholder="Поиск" style="border: none;" />
+			</div>
+		</div>
+		<table>
+			<tbody>
+				{#each objects as obj (obj.id)}
+					<tr>
+						<td style="weight=114px; font-sizt: 20px;">{obj.start_datetime}</td>
+						<td
+							class="event-tag"
+							style="text-align: center; weight=400px; font-sizt: 34px; color: rgba(0, 0, 0, 1)"
+							>{obj.name}</td
+						>
+						<td>
+							<button on:click={() => goToObjectPageMore(obj.id)} class="GOto">Перейти →</button>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/if}
 </main>
 
 <style>
+	.controls {
+		display: flex;
+		gap: 10px;
+		align-items: center;
+		margin-bottom: 20px;
+	}
+
+	.controls input {
+		flex-grow: 1;
+		padding: 5px 10px;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+	}
+
+	.controls .dropdown select {
+		padding: 8px;
+		border: none;
+		border-radius: 4px;
+		background-color: white;
+		cursor: pointer;
+	}
+
 	main {
 		margin-left: 60px;
 		margin-right: 60px;
+		font-family: "ttnormspro-regular", sans-serif;
+		color: rgba(102, 102, 102, 1);
 	}
 	header {
 		margin-top: 25px;
 		margin-left: 23px;
 		margin-right: 23px;
 		padding: 18px 37px;
-		background: linear-gradient(to right, #fccdcd, #c8aae7);
+		background: linear-gradient(to right, rgba(252, 205, 205, 1), rgba(200, 170, 231, 1));
 		border-radius: 60px;
-		height: 91;
 	}
 	.header {
 		display: flex;
@@ -167,11 +214,21 @@
 		appearance: none;
 	}
 	.logo {
-		margin-top: 7.5;
-		margin-bottom: 7.5;
-		margin-left: 19;
+		margin-top: 7.5px;
+		margin-bottom: 7.5px;
+		margin-left: 19px;
 	}
 	.nav-btn {
+		background: none;
+		border: none;
+		border-radius: 40px;
+		padding: 10px 20px;
+		cursor: pointer;
+		font-weight: 500px;
+		font-family: "ttnormspro-regular", sans-serif;
+		font-size: 18px;
+	}
+	.nav-btn.active {
 		background: white;
 		border: none;
 		border-radius: 40px;
@@ -184,17 +241,6 @@
 	.avatar {
 		margin-left: 16px;
 	}
-	.event-tag {
-		position: absolute;
-		top: 23;
-		left: 23;
-		background: #d9d9d9;
-		color: #666666;
-		padding: 7px 23px;
-		font-size: 12px;
-		border-radius: 20;
-	}
-
 	h1 {
 		color: rgba(60, 51, 64, 1);
 		font-family: "cygre-medium", sans-serif;
@@ -213,15 +259,6 @@
 			box-shadow 0.3s;
 	}
 
-	@font-face {
-		font-family: "cygre-medium";
-		src: url("/fonts/cygre-medium.ttf");
-	}
-
-	@font-face {
-		font-family: "ttnormspro-regular";
-		src: url("/fonts/ttnormspro-regular.ttf");
-	}
 	table {
 		width: 100%;
 		border-collapse: collapse;
@@ -231,24 +268,35 @@
 		background: linear-gradient(135deg, rgba(250, 202, 206, 0.5), rgba(200, 170, 231, 0.5));
 		margin-top: 22px;
 		height: 73px;
-		border-radius: 50px;
+		padding: 80.5px;
+		text-align: center;
 	}
 	table tr {
 		padding: 17px 90px;
-		text-align: left;
+		text-align: center;
 		font-size: 22px;
 		margin-right: 80px;
 	}
 	table td {
 		padding: 17px 29px;
-		text-align: left;
+		text-align: center;
 		border-bottom: 2px solid #787878;
 		font-size: 22px;
 	}
-	.filter-bar input {
-		height: 24px;
+	td select {
+		border: 2px solid rgba(120, 120, 120, 0.8);
+		border-radius: 10px;
+		padding: 9px 19px;
 	}
-	.filter-bar option {
-		height: 24px;
+
+	button {
+		background: none;
+		border: none;
+	}
+
+	.GOto {
+		border: 2px solid rgba(250, 202, 206, 0.5);
+		padding: 9px 47.75px;
+		border-radius: 20px;
 	}
 </style>
--- frontend/src/routes/create_event/+page.svelte
+++ frontend/src/routes/create_event/+page.svelte
@@ -34,9 +34,7 @@
 		tags
 	};
 
-	async function saveObject(newObject) {
-		// const newObject = { name,  start_year, start_month, start_day,start_hour, start_minute,
-		//   end_year, end_month, end_day, end_hour,end_minute,place, content, category,tags };
+	async function saveObject(newObject: any) {
 		try {
 			const response = await fetch(`${apiUrl}`, {
 				method: "POST",
@@ -56,68 +54,106 @@
 	function cancel() {
 		goto("/");
 	}
+
+	function handleDatetimeChange(event: Event) {
+		const input = event.target as HTMLInputElement;
+		const datetimeValue = input.value;
+
+		if (datetimeValue) {
+			const [datePart, timePart] = datetimeValue.split("T");
+			const [year, month, day] = datePart.split("-").map(Number);
+			const [hour, minute] = timePart.split(":").map(Number);
+			start_year = year;
+			start_month = month;
+			start_day = day;
+			start_hour = hour;
+			start_minute = minute;
+		}
+	}
+	function handleDatetimeChangeforEND(event: Event) {
+		const input = event.target as HTMLInputElement;
+		const datetimeValue = input.value;
+		if (datetimeValue) {
+			const [datePart, timePart] = datetimeValue.split("T");
+			const [year, month, day] = datePart.split("-").map(Number);
+			const [hour, minute] = timePart.split(":").map(Number);
+			end_year = year;
+			end_month = month;
+			end_day = day;
+			end_hour = hour;
+			end_minute = minute;
+		}
+	}
 </script>
 
 <header>
 	<div class="header">
-		<div class="logo"><img src="logo.svg" /></div>
+		<div class="logo"><img src="logo.svg" alt="ITAM" /></div>
 		<nav>
 			<button class="nav-btn">Мероприятия</button>
-			<button class="nav-btn">Админ-панель</button>
-			<div class="avatar"><img src="avatar.svg" /></div>
+			<button class="nav-btn" style="background: white;">Админ-панель</button>
+			<div class="avatar"><img src="avatar.svg" alt="avatar" /></div>
 		</nav>
 	</div>
 </header>
 <main>
 	<h1>Админ-панель</h1>
-	<img src="Стрелка.svg" class="back" on:click={cancel} />
-	<form on:submit|preventDefault={saveObject(newObject)}>
-		<label>Название</label>
-		<input type="text" bind:value={name} required />
-		<label>День</label>
+	<button on:click={cancel} class="back"> <img src="Стрелка.svg" alt="back" /></button>
+	<form on:submit|preventDefault={() => saveObject(newObject)}>
+		<label for="title">Название</label>
+		<input id="title" type="text" bind:value={name} required />
 		<div class="split">
-			<input type="number" bind:value={start_day} min="1" max="31" required />
-
-			<input type="number" bind:value={start_month} min="1" max="12" required />
-
-			<input type="number" bind:value={start_year} min="2024" required />
-
-			<input type="number" bind:value={start_hour} min="0" max="23" required />
-
-			<input type="number" bind:value={start_minute} min="0" max="59" required />
-		</div>
-		<label>День</label>
-		<div class="split">
-			<input type="number" bind:value={end_day} min="1" max="31" required />
-			<input type="number" bind:value={end_month} min="1" max="12" required />
-			<input type="number" bind:value={end_year} min="2024" required />
-			<input type="number" bind:value={end_hour} min="0" max="23" required />
-			<input type="number" bind:value={end_minute} min="0" max="59" required />
+			<div class="element">
+				<label for="datetimeInpuS">Начало события</label>
+				<input
+					style="width: 648px; "
+					type="datetime-local"
+					id="datetimeInputS"
+					on:input={handleDatetimeChange}
+				/>
+			</div>
+			<div class="element">
+				<label for="datetimeInputE">Конец события</label>
+				<input
+					style="width: 648px;"
+					type="datetime-local"
+					id="datetimeInputE"
+					on:input={handleDatetimeChangeforEND}
+				/>
+			</div>
 		</div>
 		<div class="split_new">
 			<div class="element">
-				<label>Тег</label><br />
-				<input type="text" bind:value={tags} required />
+				<label for="tag">Тег</label>
+				<select name="select1" id="tag" bind:value={tags}>
+					<option selected value="хакатон">хакатон</option>
+					<option value="митап">митап</option>
+					<option value="воркшоп">воркшоп</option>
+				</select>
 			</div>
 			<div class="element">
-				<label>Формат проведение</label><br />
-				<input type="text" bind:value={category} required />
+				<label for="format">Формат проведение</label>
+				<select name="select" id="format" bind:value={category}>
+					<option selected value="онлайн">онлайн</option>
+					<option value="офлайн">офлайн</option>
+					<option value="онлайн&офлайн">онлайн&офлайн</option>
+				</select>
 			</div>
 			<div class="element">
-				<label>Место проведения</label><br />
-				<input type="text" bind:value={place} required />
+				<label for="place">Место проведения</label>
+				<input id="place" type="text" bind:value={place} required />
 			</div>
 		</div>
-		<label>Описание</label>
-		<textarea bind:value={content}></textarea>
-		<button type="submit">Сохранить</button>
+		<label for="content">Описание</label>
+		<textarea bind:value={content} id="content"></textarea>
+		<div style="text-align: right;"><button type="submit" class="but">Сохранить</button></div>
 	</form>
 </main>
 
 <style>
 	.split {
 		display: grid;
-		grid-template-columns: 2fr 3fr 3fr 3fr 3fr;
+		grid-template-columns: 1fr 1fr;
 		gap: 15px;
 	}
 	.split_new {
@@ -128,8 +164,9 @@
 	main {
 		margin-left: 60px;
 		margin-right: 60px;
-		font-family: "ttnormspro-regular", sans-serif;
 		font-size: 27px;
+		font-family: "ttnormspro-regular", sans-serif;
+		color: rgba(102, 102, 102, 1);
 	}
 	label {
 		margin-bottom: 3px;
@@ -155,14 +192,17 @@
 		flex-direction: column;
 		gap: 1rem;
 	}
+	.element {
+		display: flex;
+		flex-direction: column;
+	}
 	header {
 		margin-top: 25px;
 		margin-left: 23px;
 		margin-right: 23px;
 		padding: 18px 37px;
-		background: linear-gradient(to right, #fccdcd, #c8aae7);
+		background: linear-gradient(to right, rgba(252, 205, 205, 1), rgba(200, 170, 231, 1));
 		border-radius: 60px;
-		height: 91;
 	}
 	.header {
 		display: flex;
@@ -170,16 +210,18 @@
 		align-items: center;
 	}
 	.logo {
-		margin-top: 7.5;
-		margin-bottom: 7.5;
-		margin-left: 19;
+		margin-top: 7.5px;
+		margin-bottom: 7.5px;
+		margin-left: 19px;
 	}
 	.nav-btn {
-		background: white;
 		border: none;
 		border-radius: 40px;
 		padding: 10px 20px;
 		cursor: pointer;
+		font-weight: 500px;
+		font-family: "ttnormspro-regular", sans-serif;
+		font-size: 18px;
 	}
 	nav {
 		display: flex;
@@ -196,25 +238,44 @@
 		margin-bottom: 21px;
 	}
 	button {
+		background: none;
+		border: none;
+	}
+	.but {
 		background: linear-gradient(135deg, rgba(250, 202, 206, 0.5), rgba(200, 170, 231, 0.5));
 		border: none;
-		padding: 10.5px;
+		padding: 9.5px 14px;
 		color: rgba(60, 51, 64, 1);
-		border-radius: 10px;
+		border-radius: 20px;
 		font-size: 18px;
 		cursor: pointer;
-		height: 70px;
 		transition:
 			transform 0.3s,
 			box-shadow 0.3s;
+		margin-top: 12px;
+		font-size: 25px;
+		font-weight: 400px;
+		color: rgba(60, 51, 64, 1);
+		width: 207px;
+		height: 49px;
 	}
 
-	button:hover {
+	.but:hover {
 		transform: translateY(-3px);
 		box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
 	}
-	@font-face {
-		font-family: "ttnormspro-regular";
-		src: url("/fonts/ttnormspro-regular.ttf");
+	.element select {
+		border-radius: 20px;
+		height: 78px;
+		padding: 10px 18px;
+		font-size: 27px;
+		color: #787878;
+		border: 2px solid #808080;
+	}
+	::-webkit-calendar-picker-indicator {
+		color: transparent;
+		opacity: 1;
+		background: url(/calendar.svg) no-repeat center;
+		background-size: contain;
 	}
 </style>
