7aaa74c2fef38d2be785aa41b227d401a3eae1e5
d1mspy <subbotin2126@gmail.com>
2025-03-17 14:50:15 +0300

    fix: dockerfiles, docker-compose, vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,14 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: "0.0.0.0",
+		port: 5173
+	  },
+	optimizeDeps: {
+		entries: ["src/routes/+layout.svelte", "src/routes/+page.svelte", "src/app.html"],
+		include: ["svelte", "@sveltejs/kit"]
+	},
+	publicDir: "static"
 });
